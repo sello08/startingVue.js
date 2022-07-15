0fe6582e88a1f420ccfd11688931b969cb030af2
Yusuf Akyol <yusuf.akyol@siemens.com>
2022-07-15 16:04:40 +0300

    Initial commit0

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 id="title">{{ title }}</h1>
+  <div v-if="showModal">
+    <Modal :theme="theme" @close="showModals" @isSale="changeSales">
+      <template v-slot:links>
+        <a href="#">Sign Up</a>
+        <a href="#">More Info</a>
+      </template>
+      <h1>My Prop Training</h1>
+      <p>Hello Trainees, how you doing</p>
+      <p>Hope you okay</p>
+    </Modal>
+  </div>
+  <div v-if="showModalTwo">
+    <Modal :theme="theme" @close="showModalsTwo">
+      <h1>Hello my friends again</h1>
+      <p>Hope you doing great all time</p>
+      <p>Have a nice day</p>
+    </Modal>
+  </div>
+  <button @click="showModals">Show Modal</button>
+  <button @click="showModalsTwo">Show Modal Two</button>
+  <button class="btn_2" @click="changeSale">Sale</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Modal },
+
+  data() {
+    return {
+      title: "My First Vue App :)",
+      showModal: false,
+      showModalTwo: false,
+      theme: "sale",
+    };
+  },
+  methods: {
+    showModals() {
+      this.showModal = !this.showModal;
+    },
+    showModalsTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
+    changeSale() {
+      if (this.theme == "sale") {
+        this.theme = "unsale";
+      } else {
+        this.theme = "sale";
+      }
+    },
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="backdrop">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>default slot</slot>
+
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+      <button class="btn" @click="closeModal">Close Modal</button>
+      <button class="btn_2" @click="changeSale">Sale</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+    changeSale() {
+      this.$emit("isSale");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 115px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.3);
+  width: 100%;
+  height: 100%;
+}
+.modal .actions a {
+  text-decoration: none;
+  padding: 1px;
+  border: 1px solid black;
+  border-radius: 5px;
+  color: rgb(8, 8, 4);
+  margin: 5px;
+}
+.modal.sale .actions a {
+  text-decoration: none;
+  padding: 1px;
+  border: 1px solid rgb(182, 26, 26);
+  border-radius: 5px;
+  color: rgb(182, 26, 26);
+  margin: 5px;
+}
+.modal .btn {
+  padding: px;
+  color: rgb(8, 8, 4);
+  margin: 10px;
+}
+
+.modal.sale {
+  background: rgb(255, 0, 0);
+  color: rgb(255, 255, 255);
+}
+</style>
